1d25e7feb644f2a02f230db01e778e17ab4b3ea1
Vyacheslav Ivanov <vya1977@mail.ru>
2023-10-28 17:06:15 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Promt (string message)
+{
+    Console.Write (message);
+    string value=Console.ReadLine();
+    int result=Convert.ToInt32(value);
+    return result;
+}
+
+int Zadacha (int number)
+{
+    while (number>999)
+    {
+        number=number/10;
+    }
+    return number%10;
+}
+int number = Promt("Введите число ");
+if (number < 100)
+{
+    Console.Write("Третьей цифры нет ");
+}
+else
+{
+    Console.Write(Zadacha(number));
+}
