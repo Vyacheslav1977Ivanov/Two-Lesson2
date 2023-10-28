a1b021d0c794c8347552b32ac782cd2dc9afa852
Vyacheslav Ivanov <vya1977@mail.ru>
2023-10-28 16:38:43 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите трехзначное число");
+int number=Convert.ToInt32(Console.ReadLine());
+if((number>99&&number<1000)||(number<-99&&number>-1000))
+{
+    int result=number%100/10;
+    Console.Write("Вторая цифра числа"+result);
+}
+else
+{
+    Console.Write("Неверно введено число");
+}
