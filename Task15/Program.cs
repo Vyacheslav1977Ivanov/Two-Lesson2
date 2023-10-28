1893d4390db08bbbc64d3bec2cea8485f08be15e
Vyacheslav Ivanov <vya1977@mail.ru>
2023-10-28 17:09:10 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите цифру от 1 до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 0 && num < 8 )
+{
+    if(num > 5 )
+    {
+      Console.Write("Yes");  
+    }
+    else
+    {
+      Console.Write("No");
+    }
+}
+else
+{
+    Console.Write("Такого дня недели не существует");
+}
